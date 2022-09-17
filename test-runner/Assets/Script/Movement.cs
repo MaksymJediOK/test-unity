d431457fd39c78a77dce15586284925d76ded6e6
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2022-09-17 19:16:57 +0300

    running

--- test-runner/Assets/Script/Movement.cs
+++ test-runner/Assets/Script/Movement.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 0.5f;
-    [SerializeField] float jumpHeight = 0.5f;
+    public float movementSpeed = 0.5f;
+    public float jumpHeight = 0.5f;
 
+    CapsuleCollider2D capsuleCollider;
     Rigidbody2D rbody;
 
     // Start is called before the first frame update
     void Start()
     {
-         rbody = GetComponent<Rigidbody2D>();
+        rbody = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 movementVector = new Vector2(horizontalInput * movementSpeed * 100 * Time.deltaTime, rbody.velocity.y);   
+        Vector2 movementVector = new Vector2(horizontalInput * movementSpeed * 100 * Time.deltaTime, rbody.velocity.y);
         rbody.velocity = movementVector;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
         if (Input.GetButtonDown("Jump"))
         {
-            movementSpeed = jumpHeight;
+            rbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
     }
 }
